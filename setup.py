cb06bea797ec36f439d64cec3c625f98aa11a3fc
agamboar <gamboaalvaro96@gmail.com>
2019-07-06 20:31:36 -0400

    foreign keys

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ cur.execute(sql)
 sql = """
 
 CREATE TABLE despedidos
-        (id_despido INT, rut VARCHAR(15) , PRIMARY KEY(id_despido, rut), fecha_despido DATE);      
+        (id_despido INT, rut VARCHAR(15), FOREIGN KEY(rut) REFERENCES chofer(rut), PRIMARY KEY(id_despido, rut), fecha_despido DATE);      
 
 /*TABLA RELACIONAL DE DESPEDIDOS CON CHOFER #1:1
 CREATE TABLE chofer_despidos
@@ -23,7 +23,7 @@ CREATE TABLE chofer
 
 /*TABLA RELACIONAL DE CHOFER CON CAMION #n:m*/        
 CREATE TABLE chofer_camion
-        (rut VARCHAR (10), patente VARCHAR(6));
+        (rut VARCHAR (10), patente VARCHAR(6), FOREIGN KEY(rut) REFERENCES chofer(rut), FOREIGN KEY(patente) REFERENCES camion(patente));
         
 CREATE TABLE camion
         (patente VARCHAR(6) PRIMARY KEY NOT NULL, capacidad INTEGER, fecha_ult_mantencion DATE, empresa VARCHAR (20));
@@ -34,7 +34,7 @@ CREATE TABLE destino
 
 /*TABLA RELACIONAL DE DESTINO CON BODEGAS #1:m  */
 CREATE TABLE destino_bodega
-        (cod_destino INT, cod_bodega INT);
+        (cod_destino INT, cod_bodega INT, FOREIGN KEY(cod_destino) REFERENCES destino(cod_destino), FOREIGN KEY(cod_bodega) REFERENCES bodegas(cod_bodega));
         
 CREATE TABLE bodegas
         (cod_bodega INT PRIMARY KEY NOT NULL, direccion VARCHAR(30), capacidad INT);
@@ -54,19 +54,15 @@ CREATE TABLE envios
         
 /*TABLA RELACIONAL DE ENVIOS CON CAMION  #n:m */
 CREATE TABLE envio_camion
-        (cod_envio INT, patente VARCHAR(6));
-
-/*TABLA RELACIONAL DE ENVIOS CON DETALLE ENVIOS  #n:m
-CREATE TABLE envio_detalle
-        (cod_envio INT);        
-*/        
+        (cod_envio INT, patente VARCHAR(6), FOREIGN KEY(cod_envio) REFERENCES envios(cod_envio), FOREIGN KEY(patente) REFERENCES camion(patente));
+      
 /*TABLA RELACIONAL DE ENVIOS CON CHOFER #n:m */
 CREATE TABLE envio_chofer
-        (cod_envio INT, rut VARCHAR(10));
+        (cod_envio INT, rut VARCHAR(10), FOREIGN KEY(cod_envio) REFERENCES envios(cod_envio), FOREIGN KEY(rut) REFERENCES chofer(rut));
 
 /*TABLA RELACIONAL DE ENVIOS CON DESTINO #n:1 */
 CREATE TABLE envio_destino
-        (cod_envio INT, cod_destino INT);        
+        (cod_envio INT, cod_destino INT, FOREIGN KEY(cod_envio) REFERENCES envios(cod_envio), FOREIGN KEY(cod_destino) REFERENCES destino(cod_destino));        
 
 """
 
